0b84a1259404a65cb3bd10c443fd2899c2c3c150
victoriapatarroyo <victoria.patarroyo@gmail.com>
2025-11-06 15:32:05 -0500

    Taller HTML, CSS y JavaScript

---
+++ script.js
@@ -0,0 +1,27 @@
+const valorContador = document.getElementById("contador");
+const botonIncrementar = document.getElementById("incrementar");
+const botonDecrementar = document.getElementById("decrementar");
+
+let contador = 0;
+
+botonIncrementar.addEventListener('click', function() {
+    contador++;
+    valorContador.textContent = contador;
+
+    if(valorContador.textContent >= 10) {
+        valorContador.style.color = "green";
+    } else {
+        valorContador.style.color = "black";
+    }
+});
+
+botonDecrementar.addEventListener('click', function() {
+    contador--;
+    valorContador.textContent = contador;
+
+    if(valorContador.textContent < 10) {
+        valorContador.style.color = "black";
+    } else {
+        valorContador.style.color = "green";
+    }
+});
